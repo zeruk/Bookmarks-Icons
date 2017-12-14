1e643420d2f30778d03f2460c081f9f9697bafeb
Zeruk <id.shev@yandex.ru>
2017-12-15 00:52:39 +0300

    it works!

--- popup/choose.js
+++ popup/choose.js
@@ -1,29 +1,30 @@
+
+$(function() {
+    var bookmTr = browser.bookmarks.getTree(function(tree){
+        bookm = tree[0].children[1].children;
+        console.log(bookm);
+        for(var i = 0; i<bookm.length; i++){
+            var item = bookm[i];
+            //console.log(item);
+            s = '<a to="';
+            s+=item.url;
+            s+='"><img src="';
+            s+="https://otvet.imgsmail.ru/download/u_d517c822f47a21c3bac79ab27552a9fb_800.jpg";
+            s+='"><div class="description">';
+            s+=item.title.length> 15 ? item.title.slice(0,15)+'...': item.title;
+            s+='</div></a>';
+            $(".wrapper").append(s);
+        }
+    })
+    
+    
+});
+
 $("a").bind( "click", function() {
+    alert($(this).attr("to"));
     var creating = browser.tabs.create(
         { url: $(this).attr("to")
     });
     //console.log($(this).attr("to"))
 });
-
-$(function() {
-    var bookm = [];
-    var bookmTr = browser.bookmarks.getTree(function(tree){
-        bookm = tree[0].children[1].children;
-    })
-    console.log(bookm);
-    for(var i = 0; i<bookm.length; i++){
-        item = bookm[i];
-        console.log(item);
-        s = '<a to="';
-        s+=item.url;
-        s+='"><img src="';
-        s+="https://otvet.imgsmail.ru/download/u_d517c822f47a21c3bac79ab27552a9fb_800.jpg";
-        s+='"><div class="description">';
-        s+=item.title;
-        s+='</div></a>';
-        $(".wrapper").append(s);
-    }
-    
-});
-
    
