d8919647d2ee13648a4e0bb193bdaf83fcfdfe08
Zeruk <id.shev@yandex.ru>
2017-12-15 00:37:31 +0300

    closer

--- popup/choose.js
+++ popup/choose.js
@@ -6,18 +6,23 @@ $("a").bind( "click", function() {
 });
 
 $(function() {
-    var bookm = browser.bookmarks.getChildren(5);
-    
-    bookm.foreach(function(item){
+    var bookm = [];
+    var bookmTr = browser.bookmarks.getTree(function(tree){
+        bookm = tree[0].children[1].children;
+    })
+    console.log(bookm);
+    for(var i = 0; i<bookm.length; i++){
+        item = bookm[i];
+        console.log(item);
         s = '<a to="';
-        s+=;
+        s+=item.url;
         s+='"><img src="';
-        s+=;
+        s+="https://otvet.imgsmail.ru/download/u_d517c822f47a21c3bac79ab27552a9fb_800.jpg";
         s+='"><div class="description">';
-        s+=;
+        s+=item.title;
         s+='</div></a>';
         $(".wrapper").append(s);
-    })
+    }
     
 });
 
