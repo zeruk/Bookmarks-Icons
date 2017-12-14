074659b3396c1f699afaaaa5f53e4331f3b8e17d
Zeruk <id.shev@yandex.ru>
2017-12-15 00:02:40 +0300

    closer

--- popup/choose.js
+++ popup/choose.js
@@ -3,5 +3,22 @@ $("a").bind( "click", function() {
         { url: $(this).attr("to")
     });
     //console.log($(this).attr("to"))
-   });
+});
+
+$(function() {
+    var bookm = browser.bookmarks.getChildren(5);
+    
+    bookm.foreach(function(item){
+        s = '<a to="';
+        s+=;
+        s+='"><img src="';
+        s+=;
+        s+='"><div class="description">';
+        s+=;
+        s+='</div></a>';
+        $(".wrapper").append(s);
+    })
+    
+});
+
    
