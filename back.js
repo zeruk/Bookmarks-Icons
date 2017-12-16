d729fccd804f7d585759388dbbff6cfd277ed2bb
Zeruk <id.shev@yandex.ru>
2017-12-16 21:46:38 +0300

    why new tabs doesn't open?

---
+++ back.js
@@ -0,0 +1,16 @@
+
+console.log("hello from backgr");
+
+function bmiconsOpen(UR){
+    console.log("WAU");
+    browser.tabs.update({url: UR});
+}
+
+/*
+
+,
+  "background":{
+    "scripts": ["back.js"]
+  }
+
+  */
--- popup/choose.js
+++ popup/choose.js
@@ -1,36 +1,32 @@
-
 $(function() {
+    console.log("New instance");
+    
     var bookmTr = browser.bookmarks.getTree(function(tree){
         var bookm = tree[0].children[1].children;
-        console.log(bookm[0]);
+        //console.log(bookm[0]);
         for(var i = 0; i<bookm.length; i++){
             var item = bookm[i];
             //console.log(item);
             try{
                 var s = '<a to="';
                 s+=item.url;
-                s+='"><img src="http://www.google.com/s2/favicons?domain=';
-                var slidx=item.url.indexOf("//")
+                s+='">';
+                s+='<img src="http://www.google.com/s2/favicons?domain=';
+                var slidx=item.url.indexOf("//");
                 s+=item.url.slice(slidx+2,item.url.length);
-                console.log(s);
                 s+='"><div class="description">';
                 s+=item.title.length> 15 ? item.title.slice(0,15)+'...': item.title;
                 s+='</div></a>';
+                //console.log(s);
                 $(".wrapper").append(s);
             }
             catch{}
         }
     })
-
-    
-});
-
-
-$("a").on( "click", function() {
-    console.log("hello");
-    var creating = browser.tabs.create(
-        { url: $(this).attr("to")
+    $('a').bind("click", function(){
+        console.log("WAU");
+        browser.tabs.update({url: $(this).attr("to")});
     });
-    //console.log($(this).attr("to"))
+    console.log("binded");
 });
    
