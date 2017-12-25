6dc4c000cca5158345f21df8567f1f26d0cc2d50
Zeruk <id.shev@yandex.ru>
2017-12-25 22:48:57 +0300

    folders

--- popup/choose.js
+++ popup/choose.js
@@ -3,25 +3,30 @@ var bookmTr = browser.bookmarks.getTree(function(tree){
     for(var i = 0; i<bookm.length; i++){
         var item = bookm[i];
         try{
-            var aEl = document.createElement("a");
-            aEl.setAttribute("to",item.url);
-            aEl.onclick = function() {
-                var creating = browser.tabs.create(
-                    { url: this.getAttribute("to")}
-                );
-            };
+            if(item.url == undefined){ 
+                console.log(item.title);
+            }
+            else{
+                var aEl = document.createElement("a");
+                aEl.setAttribute("to",item.url);
+                aEl.onclick = function() {
+                    var creating = browser.tabs.create(
+                        { url: this.getAttribute("to")}
+                    );
+                };
 
-            var slidx=item.url.indexOf("//");            
-            var imgEl = document.createElement("img");
-            imgEl.setAttribute("src", "http://www.google.com/s2/favicons?domain="+item.url.slice(slidx+2,item.url.length));
+                var slidx=item.url.indexOf("//");            
+                var imgEl = document.createElement("img");
+                imgEl.setAttribute("src", "http://www.google.com/s2/favicons?domain="+item.url.slice(slidx+2,item.url.length));
 
-            var descEl = document.createElement("div");
-            descEl.classList.add('description');
-            descEl.innerText = item.title.length> 15 ? item.title.slice(0,15)+'...': item.title;
+                var descEl = document.createElement("div");
+                descEl.classList.add('description');
+                descEl.innerText = item.title.length> 15 ? item.title.slice(0,15)+'...': item.title;
 
-            aEl.appendChild(imgEl);
-            aEl.appendChild(descEl);
-            document.getElementById("wrapper").appendChild(aEl);
+                aEl.appendChild(imgEl);
+                aEl.appendChild(descEl);
+                document.getElementById("wrapper").appendChild(aEl);
+            }
         }
         catch(err){
             ;
