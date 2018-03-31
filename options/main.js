9ab1f8543eef783f63fa8bdcac31f2d121d18fe4
Ilya Shevchenko <id.shev@yandex.ru>
2018-04-01 00:45:46 +0300

    new icons!

--- options/main.js
+++ options/main.js
@@ -1,2 +1,14 @@
-var mLS = window.localStorage;
-mLS.setItem("BINewWindowButton","2");
+document.addEventListener('DOMContentLoaded', function(){
+    Array.prototype.forEach.call(document.querySelectorAll("#form_error_message_frontend + div > div:last-child label"), function(e){
+        var mLS = window.localStorage;
+        if(mLS.getItem("BINewWindowButton")==2)
+            e.setAttribute("checked","true");
+        console.log(e);
+    });
+});
+
+function check(n){
+    var mLS = window.localStorage;
+    mLS.setItem("BINewWindowButton",string(n));
+    console.log(n);
+}
